13de567aa341039bb8f09f13abf8a7fb4b1b8566
Oliver Dawson <oliver.dawson2015@my.ntu.ac.uk>
2017-04-30 17:54:44 +0100

    Fixed Player class merge error

--- src/match3Project/match3Project/PlayerClass.h
+++ src/match3Project/match3Project/PlayerClass.h
@@ -6,19 +6,25 @@ class playerClass
 private:
 	std::string profile;
 	std::string character;
-	std::string getProfileName();
-	playerClass(std::string profileName, std::string characterName);
-	std::string getCharacterName();
+	
 protected:
 	int hp, red, blue, green, yellow;
 public:
 	
 	playerClass();
+	playerClass(std::string profileName, std::string characterName);
+	
 	void adjustHp(int);
 	void adjustRed(int);
 	void adjustBlue(int);
 	void adjustYellow (int);
 	void adjustGreen(int);
-	
+	int getHp();
+	int getRed();
+	int getBlue();
+	int getYellow();
+	int getGreen();
+	std::string getProfileName();	
+	std::string getCharacterName();
 	
 };
