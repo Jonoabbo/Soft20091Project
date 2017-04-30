5b1a431081d27032c3ae522af225f0d3dafd5bf2
Oliver Dawson <oliver.dawson2015@my.ntu.ac.uk>
2017-04-30 18:31:33 +0100

    Save functionality implemented

--- src/match3Project/match3Project/SaveBoard.cpp
+++ src/match3Project/match3Project/SaveBoard.cpp
@@ -12,22 +12,22 @@ saveBoard::saveBoard(playerClass player, enemyClass enemy, string board)
 
 	//Save profile data
 	saveData.push_back("<profile>");
-	saveData.push_back(player.getProfileName());
+	saveData.push_back(player.getProfileName);
 	saveData.push_back("<character>");
 	saveData.push_back(player.getCharacterName);
 	saveData.push_back("<playerData>");
-	saveData.push_back(player.getHp());
-	saveData.push_back(player.getRed());
-	saveData.push_back(player.getBlue());
-	saveData.push_back(player.getYellow());
+	saveData.push_back(stoi(player.getHp));
+	saveData.push_back(stoi(player.getRed));
+	saveData.push_back(stoi(player.getBlue));
+	saveData.push_back(stoi(player.getYellow));
 	saveData.push_back(player.getGreen());
 	saveData.push_back("</playerData>");
 	saveData.push_back("<enemyData>");
-	saveData.push_back(enemy.getHp());
-	saveData.push_back(enemy.getRed());
-	saveData.push_back(enemy.getBlue());
-	saveData.push_back(enemy.getYellow());
-	saveData.push_back(enemy.getGreen());
+	saveData.push_back(stoi(enemy.getHp));
+	saveData.push_back(stoi(enemy.getRed));
+	saveData.push_back(stoi(enemy.getBlue));
+	saveData.push_back(stoi(enemy.getYellow));
+	saveData.push_back(stoi(enemy.getGreen));
 	saveData.push_back("</enemyData>");
 	saveData.push_back("<board>");
 	saveData.push_back(board);
@@ -40,16 +40,31 @@ saveBoard::saveBoard(playerClass player, enemyClass enemy, string board)
 
 void saveBoard::save(vector<string> saveData) {
 	string line;
-	ifstream myFile;
-	myFile.open("gameSaves.xml");
+	vector<string> temp;
+	fstream myFile;
+	myFile.open("gameSaves.xml", ios::in);
 	if (myFile.is_open())
 	{
 		while (getline(myFile, line))
 		{
 			if (line = "<root>")
 			{
-
+				for (int n = 0; n < saveData.size(); n++)
+				{
+					temp.push_back(saveData[n]);
+				}
 			}
+			temp.push_back(line);
+		}
+	}
+	myFile.close;
+
+	myFile.open("gamesSaves.xml", ios::out);
+	if (myFile.is_open())
+	{
+		for (int n = 0; n < temp.size(); n++)
+		{
+			myFile << temp[n];
 		}
 	}
 }
