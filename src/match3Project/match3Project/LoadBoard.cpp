a62a4ca040edb17e42661078175c45bef3ede7aa
Oliver Dawson <oliver.dawson2015@my.ntu.ac.uk>
2017-05-02 18:10:03 +0100

    load now correctly adds profile and character names

--- src/match3Project/match3Project/LoadBoard.cpp
+++ src/match3Project/match3Project/LoadBoard.cpp
@@ -26,6 +26,7 @@ saveFile loadBoard::loadCharacter(string profileName, string charName) {
 				getline(saveFile, line);
 				if (line == profileName)
 				{
+					loadedFile.player.setProfileName(profileName);
 					//go through every line in the profile
 					while (line != "</profile>")
 					{
@@ -37,6 +38,7 @@ saveFile loadBoard::loadCharacter(string profileName, string charName) {
 							getline(saveFile, line);
 							if (line == charName)
 							{
+								loadedFile.player.setCharacterName(charName);
 								while (line != "</character>")
 								{
 									getline(saveFile, line);
@@ -102,99 +104,91 @@ saveFile loadBoard::getHardest(string profileName) {
 			if (line == "<profile>")
 			{
 				getline(saveFile, line);
-				if (line == profileName)
+				while (line != "</profile>")
 				{
-					profileFound = true;
-					break;
-				}
-			}
-		}
 
-		if (profileFound == true)
-		{
-			while (line != "</profile>")
-			{
-
-				getline(saveFile, line);
-				if (line == "<enemyData>")
-				{
-					counter++;
 					getline(saveFile, line);
-					//The lower the number the harder game
-					if (stoi(line) < hardest)
+					if (line == "<enemyData>")
 					{
-						position = counter;
-						hardest = stoi(line);
-					}
-				}
-			}
-		}
-		saveFile.close();
-
-		counter = 0;
-		saveFile.open("gameSaves.xml", ios::in);
-		if (saveFile.is_open())
-		{
-			//Look through each line until the profile is found
-			while (getline(saveFile, line))
-			{
-				if (line == "<profile>")
-				{
-					getline(saveFile, line);
-					if (line == profileName)
-					{
-						break;
-					}
-				}
-			}
-			while (line != "</profile>")
-			{
-				getline(saveFile, line);
-				if (line == "<character>")
-				{
-					counter++;
-					if (counter == position)
-					{
-						while (line != "</character>")
+						counter++;
+						getline(saveFile, line);
+						//The lower the number the harder game
+						if (stoi(line) < hardest)
 						{
-							getline(saveFile, line);
-							if (line == "<playerData>")
-							{
-								getline(saveFile, line);
-								loadedFile.player.adjustHp(stoi(line));
-								getline(saveFile, line);
-								loadedFile.player.adjustRed(stoi(line));
-								getline(saveFile, line);
-								loadedFile.player.adjustBlue(stoi(line));
-								getline(saveFile, line);
-								loadedFile.player.adjustYellow(stoi(line));
-								getline(saveFile, line);
-								loadedFile.player.adjustGreen(stoi(line));
-							}
-							else if (line == "<enemyData>")
-							{
-								getline(saveFile, line);
-								loadedFile.enemy.adjustDifficulty(stoi(line));
-								getline(saveFile, line);
-								loadedFile.enemy.adjustHp(stoi(line));
-								getline(saveFile, line);
-								loadedFile.enemy.adjustRed(stoi(line));
-								getline(saveFile, line);
-								loadedFile.enemy.adjustBlue(stoi(line));
-								getline(saveFile, line);
-								loadedFile.enemy.adjustYellow(stoi(line));
-								getline(saveFile, line);
-								loadedFile.enemy.adjustGreen(stoi(line));
-							}
-							else if (line == "<board>")
-							{
-								getline(saveFile, line);
-								loadedFile.board = line;
-							}
+							position = counter;
+							hardest = stoi(line);
 						}
 					}
 				}
+			}
+		}
+	}
+	saveFile.close();
 
+			counter = 0;
+			saveFile.open("gameSaves.xml", ios::in);
+			if (saveFile.is_open())
+			{
+				//Look through each line until the profile is found
+				while (getline(saveFile, line))
+				{
+					if (line == "<profile>")
+					{
+						getline(saveFile, line);
+						loadedFile.player.setProfileName(line);
+						while (line != "</profile>")
+						{
+							getline(saveFile, line);
+							if (line == "<character>")
+							{
+								getline(saveFile, line);
+								loadedFile.player.setCharacterName(line);
+								counter++;
+								if (counter == position)
+								{
+									while (line != "</character>")
+									{
+										getline(saveFile, line);
+										if (line == "<playerData>")
+										{
+											getline(saveFile, line);
+											loadedFile.player.adjustHp(stoi(line));
+											getline(saveFile, line);
+											loadedFile.player.adjustRed(stoi(line));
+											getline(saveFile, line);
+											loadedFile.player.adjustBlue(stoi(line));
+											getline(saveFile, line);
+											loadedFile.player.adjustYellow(stoi(line));
+											getline(saveFile, line);
+											loadedFile.player.adjustGreen(stoi(line));
+										}
+										else if (line == "<enemyData>")
+										{
+											getline(saveFile, line);
+											loadedFile.enemy.adjustDifficulty(stoi(line));
+											getline(saveFile, line);
+											loadedFile.enemy.adjustHp(stoi(line));
+											getline(saveFile, line);
+											loadedFile.enemy.adjustRed(stoi(line));
+											getline(saveFile, line);
+											loadedFile.enemy.adjustBlue(stoi(line));
+											getline(saveFile, line);
+											loadedFile.enemy.adjustYellow(stoi(line));
+											getline(saveFile, line);
+											loadedFile.enemy.adjustGreen(stoi(line));
+										}
+										else if (line == "<board>")
+										{
+											getline(saveFile, line);
+											loadedFile.board = line;
+										}
+										
+									}
+								}
+							}
+						}
+					
+				
 			}
 		}
 
@@ -203,3 +197,5 @@ saveFile loadBoard::getHardest(string profileName) {
 
 	return loadedFile;
 }
+
+
